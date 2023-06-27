9a1d46b4a876ba230eb0dfe11452bdfe8da34c5d
Saajan Singh Rana <123057148+saajanrana@users.noreply.github.com>
2023-06-27 04:30:23 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,48 +1,13 @@
- const msg = new SpeechSynthesisUtterance();
-  let voices = [];
-  const voicesDropdown = document.querySelector('[name="voice"]');
-  const options = document.querySelectorAll('[type="range"], [name="text"]');
-  const speakButton = document.querySelector('#speak');
-  const stopButton = document.querySelector('#stop');
+const text = document.querySelector('textarea');
+const start = document.getElementById('speak');
+const stop = document.getElementById('stop');
 
-  // Fetch and load available voices
-  function populateVoices() {
-    voices = speechSynthesis.getVoices();
-    voicesDropdown.innerHTML = voices
-      .filter(voice => voice.lang.includes('en')) // Filter for English voices
-      .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
-      .join('');
-  }
+let utterance = new SpeechSynthesisUtterance(text.value);
 
-  // Set the voice for speech synthesis
-  function setVoice() {
-    const selectedVoice = voices.find(voice => voice.name === voicesDropdown.value);
-    msg.voice = selectedVoice;
-  }
+start.addEventListener('click', () => {
+	window.speechSynthesis.speak(utterance); 
+});
 
-  // Toggle speaking
-  function toggleSpeech() {
-    if (speechSynthesis.speaking) {
-      speechSynthesis.cancel();
-    } else {
-      setVoice();
-      speechSynthesis.speak(msg);
-    }
-  }
-
-  // Set speech options when the rate, pitch, or text changes
-  function setSpeechOptions() {
-    msg.rate = options[0].value;
-    msg.pitch = options[1].value;
-    msg.text = options[2].value;
-  }
-
-  // Event listeners
-  speechSynthesis.addEventListener('voiceschanged', populateVoices);
-  voicesDropdown.addEventListener('change', setVoice);
-  options.forEach(option => option.addEventListener('change', setSpeechOptions));
-  speakButton.addEventListener('click', toggleSpeech);
-  stopButton.addEventListener('click', () => speechSynthesis.cancel());
-
-  // Initialize
-  populateVoices();
+stop.addEventListener('click', () => {
+	window.speechSynthesis.cancel()
+});
